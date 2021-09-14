2461969e0c75cedd5e3e9e5a214b854e257b6a4f
ardanyyy <ardany@mesoamericana.edu.gt>
2021-09-14 13:57:01 -0600

    Paginado Productos filtrado de 10 en 10

--- src/app/views/pageTienda/productos/productos.component.ts
+++ src/app/views/pageTienda/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { producto } from '../interfaces/respuestaProductosInterface';
 import { ProductosService } from '../../services/productos.service';
+import { InterceptorsService } from '../../pageAdminTienda/interceptors/interceptors.service';
 
 @Component({
   selector: 'app-productos',
@@ -11,14 +12,40 @@ import { ProductosService } from '../../services/productos.service';
 export class ProductosComponent implements OnInit {
 
   cargando =false;
+  contador= 10;
   productos: producto []= [];
+  dataFiltrada: producto[] = [];
   constructor(private productoService: ProductosService) { }
 
   ngOnInit(): void {
      this.productoService.getLIstarProductos().subscribe(resp=> {
-       this.productos = resp.data;
+       this.productos  = resp.data;
+       this.dataFiltrada = resp.data.slice(0,10);
        this.cargando= true;
      })
+     this.probandoScrollInfinito();
   }
+
+
+  probandoScrollInfinito()
+  {
+
+
+      let observe = new IntersectionObserver(entries => {
+        if(this.contador <= this.productos.length )
+        {
+          if(entries[0].isIntersecting)
+          {
+            setTimeout(() => {
+              this.contador+=10;
+              this.dataFiltrada = this.productos.slice(0,this.contador);
+            }, 1000);
+          }
+        }
+        return ;
+
+    });
+    observe.observe(document.querySelector('.footer')!);
+    }
 
 }
