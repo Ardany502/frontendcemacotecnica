8f1f022459582d3b70aa5f9c4cb64132b4cdb9dc
ardanyyy <ardany@mesoamericana.edu.gt>
2021-09-14 21:03:09 -0600

    Configuracion Envaiorment apiBackend Ruta

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://laravelbackend502.herokuapp.com/api',
-  base_url_img: 'https://laravelbackend502.herokuapp.com',
+  base_url: 'https://backendcemacoprueba.herokuapp.com/api',
+  base_url_img: 'https://backendcemacoprueba.herokuapp.com',
 };
